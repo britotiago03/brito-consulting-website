511fe705ace41d75b5ce97007c3818f1d4f5a276
Tiago Brito <69848652+britotiago03@users.noreply.github.com>
2024-07-12 17:03:50 -0400

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
 
 const App = () => {
     return (
-        <div className="App">
-            <h1>Welcome to Brito Consulting</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                </Routes>
+                <Footer />
+            </div>
+        </Router>
     );
 };
 
