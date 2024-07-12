fb7a763349a092838e1d59c2e589fbaa1a0c97b6
Tiago Brito <69848652+britotiago03@users.noreply.github.com>
2024-07-12 17:00:31 -0400

    Created Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import HeroSection from '../components/home_page/HeroSection';
+import ServicesSection from '../components/home_page/ServicesSection';
+import UniqueSellingPoints from '../components/home_page/UniqueSellingPoints';
+import Process from '../components/home_page/Process';
+import Testimonials from '../components/home_page/Testimonials';
+import Contact from '../components/home_page/Contact';
+
+const Home = () => {
+    return (
+        <main>
+            <HeroSection />
+            <ServicesSection />
+            <UniqueSellingPoints />
+            <Process />
+            <Testimonials />
+            <Contact />
+        </main>
+    );
+};
+
+export default Home;
