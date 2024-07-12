20161b0c880fc3c8d2433ac80e885db11b3fafe5
Tiago Brito <69848652+britotiago03@users.noreply.github.com>
2024-07-12 18:01:39 -0400

    Updated Contact.js

--- src/components/home_page/Contact.js
+++ src/components/home_page/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Contact.css';
 import tiagoBritoImage from '../../assets/tiago-brito.jpg'; // Adjust the path if necessary
+import { Link } from 'react-router-dom';
 
 const Contact = () => {
     return (
@@ -12,7 +13,9 @@ const Contact = () => {
                     <input type="text" placeholder="Your Name" className="contact-input" />
                     <input type="email" placeholder="Your Email" className="contact-input" />
                     <textarea placeholder="Your Message" className="contact-textarea"></textarea>
-                    <button type="submit" className="contact-button">Submit Inquiry</button>
+                    <Link to="/inquiry-received" className="contact-button-link">
+                        <button type="button" className="contact-button" onClick={() => window.scrollTo(0, 0)}>Submit Inquiry</button>
+                    </Link>
                 </form>
                 <div className="contact-info">
                     <img src={tiagoBritoImage} alt="Tiago Brito" className="contact-image" />
