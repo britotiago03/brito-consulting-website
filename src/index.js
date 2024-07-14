dd6501e1f9dc9a9f10e219913f482efaaf9795a2
Ruben Teixeira <devruben10@gmail.com>
2024-07-14 17:52:34 +0100

    test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import './styles/tailwind.css';
 
-// test
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
